b05cd55d75c1328b9a001587d73c394d47769900
drklrd <srvbhattarai@gmail.com>
2017-08-03 21:49:12 +0545

    re-introduce input field as controlled-component

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -2,11 +2,22 @@ import React, { Component } from 'react';
 
 class SearchBar extends Component {
 
+    constructor(props){
+        super(props);
+        this.state = {
+            term : ''
+        }
+    }
+
     render(){
         return (
-                <div>
-                    <input  placeholder="Search here" />
-                </div>
+            <div>
+                <input
+                placeholder="Search here"
+                value = {this.state.term}
+                onChange={event => this.setState({term : event.target.value}) }
+                />
+            </div>
         );
     }
 }
