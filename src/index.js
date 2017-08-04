2352e7341ccbc0499825e7259905875091d6c366
drklrd <srvbhattarai@gmail.com>
2017-08-04 22:51:42 +0545

    search videos

--- src/index.js
+++ src/index.js
@@ -16,17 +16,23 @@ class App extends Component {
             videos : [],
             selectedVideo : null
         };
-        YTSearch({key : API_KEY , term : 'nepal'},(videos) => {
+
+        this.videoSearch('nepal trekking');
+    }
+
+    videoSearch(term){
+        YTSearch({key : API_KEY , term : term},(videos) => {
             this.setState({
                 videos,
                 selectedVideo : videos[0]
             });
         });
     }
+
     render(){
         return (
             <div>
-                <SearchBar />
+                <SearchBar onSearchTermChange = { term => this.videoSearch(term)} />
                 <VideoDetail video={this.state.selectedVideo} />
                 <VideoList  onVideoSelect={selectedVideo => this.setState({selectedVideo})} videos = {this.state.videos} />
             </div>
