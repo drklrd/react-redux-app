dc16d609562b61b25af781e58e40453337931683
drklrd <srvbhattarai@gmail.com>
2017-08-04 22:11:54 +0545

    add iframe for youtube video

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 
 const API_KEY = 'AIzaSyCo_uI4yg7cOjZEUZOEYEKwH6_Fkg2PIfc';
 
@@ -11,20 +12,18 @@ class App extends Component {
 
     constructor(props){
         super(props);
-
         this.state = {
             videos : []
         };
-
         YTSearch({key : API_KEY , term : 'nepal'},(videos) => {
             this.setState({ videos });
         });
     }
-
     render(){
         return (
             <div>
                 <SearchBar />
+                <VideoDetail video={this.state.videos[0]} />
                 <VideoList videos = {this.state.videos} />
             </div>
         );
