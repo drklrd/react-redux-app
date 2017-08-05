5438afbbbdb48c0f6deb9ca5533589496bf7d862
drklrd <srvbhattarai@gmail.com>
2017-08-05 20:25:28 +0545

    middleware - redux promise magic

--- bookapp_src/reducers/index.js
+++ bookapp_src/reducers/index.js
@@ -1,3 +1,5 @@
+// reducer is a function that returns a piece of app state
+
 import { combineReducers } from 'redux';
 
 import BooksReducer from './reducer_books';
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,8 @@
 import { combineReducers } from 'redux';
+import WeatherReducer from './reducer_weather';
 
 const rootReducer = combineReducers({ //wiring reducers to our app.
-    state : (state = {} ) => state
+    weather : WeatherReducer
 });
 
 export default rootReducer;
