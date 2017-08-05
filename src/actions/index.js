8a2abcd8d0d6151935db53ecea2bf342b5182f16
drklrd <srvbhattarai@gmail.com>
2017-08-05 11:52:30 +0545

    create an action

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,9 @@
 //action creator is just a function
 
 export function selectBook(book){
-    console.log('$$',book.title);
+    // selectBook is an Action creatior and needs to return an object with a type property
+    return {
+        type : 'BOOK_SELECTED',
+        payload : book
+    };
 }
--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -1,12 +1,15 @@
 export default function() {
     return  [
         {
+            id : 1,
             title : 'Book 1'
         },
         {
+            id : 2,
             title : 'Book 2'
         },
         {
+            id : 3,
             title : 'Book 3'
         }
     ];
