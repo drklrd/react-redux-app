c5b9de5d258e519ea04756d249f72966edd06f15
drklrd <srvbhattarai@gmail.com>
2017-08-05 20:32:05 +0545

    avoid state mutation in reducers

--- src/reducers/reducer_weather.js
+++ src/reducers/reducer_weather.js
@@ -1,4 +1,12 @@
-export default function(state=null,action){
-    console.log('Action receiver',action);
+import { FETCH_WEATHER } from '../actions/index';
+
+export default function(state=[],action){
+    switch (action.type){
+        case FETCH_WEATHER :
+            // NEVER !!!
+            // return state.push(action.payload.data); NEVER DO THIS. NEVER MUTATE ARRAY !!!!
+            // return state.concat([action.payload.data]); // THIS IS GOOD.
+            return [action.payload.data, ...state]; // OR THE ES6 way
+    }
     return state;
 }
