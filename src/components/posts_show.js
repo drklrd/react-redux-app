1646e5631c6aa5e54a42a9e86ba6188949548a47
drklrd <srvbhattarai@gmail.com>
2017-08-06 23:33:23 +0545

    selecting from ownProps

--- src/components/posts_show.js
+++ src/components/posts_show.js
@@ -1,7 +1,16 @@
 import React , { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchPost} from '../actions';
 
 class PostsShow extends Component{
-     render(){
+
+    componentDidMount(){
+        const { id } = this.props.match.params;
+        // ownProps is absolutely equal to this.props for this component
+
+        this.props.fetchPost(id);
+    }
+    render(){
          return(
              <div>
                  POST
@@ -10,4 +19,11 @@ class PostsShow extends Component{
      }
 }
 
-export default PostsShow;
+function mapStateToProps({ posts }, ownProps){
+    // ownProps is absolutely equal to this.props for this component
+    return {
+        post : posts[ownProps.match.params.id]
+    };
+}
+
+export default connect(mapStateToProps,{ fetchPost })(PostsShow);
