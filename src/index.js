f49ec064a54adc379ff313d27ed47a3dee012351
drklrd <srvbhattarai@gmail.com>
2017-08-06 21:02:34 +0545

    dummy react router

--- src/index.js
+++ src/index.js
@@ -4,13 +4,36 @@ import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import ReduxPromise from 'redux-promise';
 
+
+
 import App from './components/app';
 import reducers from './reducers';
 
 const createStoreWithMiddleware = applyMiddleware(ReduxPromise)(createStore);
 
+
+import { BrowserRouter, Route   }  from 'react-router-dom';
+
+class Hello extends React.Component {
+    render(){
+        return <div>Hello</div>
+    }
+}
+
+class Good extends React.Component {
+    render(){
+        return <div>Good</div>
+    }
+}
+
+
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
+    <BrowserRouter>
+        <div>
+            <Route path="/hello" component={Hello} />
+            <Route path="/good" component={Good} />
+        </div>
+    </BrowserRouter>
   </Provider>
   , document.querySelector('.container'));
