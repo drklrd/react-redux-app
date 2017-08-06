cf2fb5b6fc5ad04bde1c5b6f2cff2b7da63ab32a
drklrd <srvbhattarai@gmail.com>
2017-08-06 23:21:41 +0545

    receiving new props with tylish ES6

--- src/reducers/reducer_posts.js
+++ src/reducers/reducer_posts.js
@@ -1,4 +1,4 @@
-import { FETCH_POSTS } from '../actions';
+import { FETCH_POSTS, FETCH_POST } from '../actions';
 import _ from 'lodash';
 
 export default function(state={},action){
@@ -6,6 +6,16 @@ export default function(state={},action){
     switch(action.type){
         case FETCH_POSTS:
             return _.mapKeys(action.payload.data,'id');
+            break;
+        case FETCH_POST :
+            // const post = action.payload.data;
+            // const newState = { ...state };
+            // newState[post.id] = post;
+            // return newState;
+            // or better in es6
+            return {
+                ...state, [action.payload.data.id] : action.payload.data
+            };
             break;
         default:
             return state;
