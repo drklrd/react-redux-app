6ae0f116b86d3183a4f8c44a523c1dc6d5f017be
drklrd <srvbhattarai@gmail.com>
2017-08-06 21:55:10 +0545

    add new route comp with a BUGGGGGGGGGG BUGGG BUGGGG

---
+++ src/components/posts_new.js
@@ -0,0 +1,12 @@
+import React , { Component } from 'react';
+
+export default class PostsNew extends Component{
+
+    render(){
+        return (
+            <div>
+                POST A NEW
+            </div>
+        );
+    }
+}
